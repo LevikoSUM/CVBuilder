2af9e3a7e1220c827369607d5ab4fe36815ef33c
LevikoSUM <115878070+LevikoSUM@users.noreply.github.com>
2023-07-08 22:43:34 +0300

    Add project

---
+++ CVBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CVBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

2b13c05e2431bbe20f8fe68ce87c46da68f11ae9
Leviko <kosta.ilev@gmail.com>
2023-07-08 23:09:39 +0300

    Add Education class

---
+++ CVBuilder/Models/Education.cs
@@ -0,0 +1,10 @@
+﻿namespace CVBuilder.Models
+{
+    public class Education
+    {
+        public int EducationId { get; set; }
+        public string EducationName { get; set; }
+        public string InstituteName { get; set; }
+        public string EducationLevel { get; set; }
+    }
+}

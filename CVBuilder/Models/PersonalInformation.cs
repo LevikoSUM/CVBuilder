bc1e2307f820f1df3262b7e36c699ab6024299c1
Leviko <kosta.ilev@gmail.com>
2023-07-08 23:12:12 +0300

    Add PersonalInformation class

---
+++ CVBuilder/Models/PersonalInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace CVBuilder.Models
+{
+    public class PersonalInformation
+    {
+        public int PersonalInformationId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMail { get; set; }
+    }
+}

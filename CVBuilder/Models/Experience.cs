606f95e9b717fad5c6509d221a7d6ba8d842da9b
Leviko <kosta.ilev@gmail.com>
2023-07-08 23:07:28 +0300

    Add Experience class

---
+++ CVBuilder/Models/Experience.cs
@@ -0,0 +1,13 @@
+﻿namespace CVBuilder.Models
+{
+    public class Experience
+    {
+        public int ExperienceId { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set;}
+        public DateTime StartDate { get; set;}
+        public DateTime EndDate { get; set;}
+        public string Responsibility { get; set;}
+        public string Achievement { get; set; }
+    }
+}

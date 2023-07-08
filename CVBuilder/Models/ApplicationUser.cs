f1aa511b75ab7770835c3090e2462cc403f5adb6
Leviko <kosta.ilev@gmail.com>
2023-07-08 23:01:32 +0300

    Add ApplicationUser

---
+++ CVBuilder/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CVBuilder.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nickname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}

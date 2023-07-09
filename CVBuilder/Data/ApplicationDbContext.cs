7cc42f1c32b833091546f745818345e9ae995db4
Leviko <kosta.ilev@gmail.com>
2023-07-09 22:00:36 +0300

    Add relationships in dbcontext

--- CVBuilder/Data/ApplicationDbContext.cs
+++ CVBuilder/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CVBuilder.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CVBuilder.Data
@@ -8,6 +9,40 @@ namespace CVBuilder.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<CV> CVs { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<PersonalInformation> PersonalInformations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CV>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.CVs)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CV>()
+                .HasOne(cv => cv.PersonalInformation)
+                .WithOne(pi => pi.CV)
+                .HasForeignKey<PersonalInformation>(pi => pi.CVId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CV>()
+                .HasMany(cv => cv.Educations)
+                .WithOne(e => e.CV)
+                .HasForeignKey(e => e.CVId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CV>()
+                .HasMany(cv => cv.Experiences)
+                .WithOne(e => e.CV)
+                .HasForeignKey(e => e.CVId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }

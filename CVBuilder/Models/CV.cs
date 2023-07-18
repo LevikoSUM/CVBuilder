d6287bb7192044a4b0f7aeb6e0249e85e4f9bd83
Leviko <kosta.ilev@gmail.com>
2023-07-18 22:06:51 +0300

    update cv model

--- CVBuilder/Models/CV.cs
+++ CVBuilder/Models/CV.cs
@@ -9,10 +9,23 @@ namespace CVBuilder.Models
         public string Name { get; set; }
         public string? UserId { get; set; }
         public ApplicationUser? User { get; set; }
-        public ICollection<Experience>? Experiences { get; set;}
-        public ICollection<Education>? Educations { get; set;}
-        public int? PersonalInformationId { get; set; }
-        public PersonalInformation? PersonalInformation { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public int? Age { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? EMail { get; set; }
+        public string? EducationName { get; set; }
+        public string? InstituteName { get; set; }
+        public string? EducationLevel { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Position { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Responsibility { get; set; }
+        public string? Achievement { get; set; }
+        public int? TemplateId { get; set; }
+        public Template? Template { get; set; }
     }
 
 }

fc5224805fc75e93049ac640db4da34297967147
dist <120486581+dimitarpst@users.noreply.github.com>
2023-07-18 23:03:30 +0300

    commit

--- CVBuilder/Controllers/HomeController.cs
+++ CVBuilder/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace CVBuilder.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();

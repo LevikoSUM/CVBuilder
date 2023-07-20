84244ecb1f284ab38436c460b830b94d4c3ea4b4
dist <120486581+dimitarpst@users.noreply.github.com>
2023-07-20 20:27:22 +0300

    -added create cv

---
+++ CVBuilder/Data/Migrations/20230720144903_CVupdateUI.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVBuilder.Data.Migrations
+{
+    public partial class CVupdateUI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Achievement",
+                table: "CVs");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "CVs");
+
+            migrationBuilder.DropColumn(
+                name: "EducationLevel",
+                table: "CVs");
+
+            migrationBuilder.DropColumn(
+                name: "EducationName",
+                table: "CVs");
+
+            migrationBuilder.RenameColumn(
+                name: "EMail",
+                table: "CVs",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "Responsibility",
+                table: "CVs",
+                newName: "University");
+
+            migrationBuilder.RenameColumn(
+                name: "Position",
+                table: "CVs",
+                newName: "Phone");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "CVs",
+                newName: "Jobtitle");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "CVs",
+                newName: "FullName");
+
+            migrationBuilder.RenameColumn(
+                name: "InstituteName",
+                table: "CVs",
+                newName: "Degree");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "CVs",
+                newName: "Company");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GraduationYear",
+                table: "CVs",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GraduationYear",
+                table: "CVs");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "CVs",
+                newName: "EMail");
+
+            migrationBuilder.RenameColumn(
+                name: "University",
+                table: "CVs",
+                newName: "Responsibility");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "CVs",
+                newName: "Position");
+
+            migrationBuilder.RenameColumn(
+                name: "Jobtitle",
+                table: "CVs",
+                newName: "PhoneNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "CVs",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Degree",
+                table: "CVs",
+                newName: "InstituteName");
+
+            migrationBuilder.RenameColumn(
+                name: "Company",
+                table: "CVs",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Achievement",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EducationLevel",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EducationName",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- CVBuilder/wwwroot/js/dashboard.js
+++ CVBuilder/wwwroot/js/dashboard.js
@@ -25,42 +25,158 @@ window.addEventListener('resize', () => {
     }
 });
 
-
 const cvForm = document.getElementById('cv-form');
 cvForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const formData = new FormData(cvForm);
+    const cvName = cvForm.querySelector('input[name="cvname"]').value;
+    const fullName = cvForm.querySelector('input[name="fullname"]').value;
+    const email = cvForm.querySelector('input[name="email"]').value;
+    const gender = cvForm.querySelector('input[name="gender"]').value;
+    const age = cvForm.querySelector('input[name="age"]').value;
+    const phone = cvForm.querySelector('input[name="phone"]').value;
+    const address = cvForm.querySelector('input[name="address"]').value;
+    const jobTitle = cvForm.querySelector('input[name="job-title"]').value;
+    const company = cvForm.querySelector('input[name="company"]').value;
+    const startDate = cvForm.querySelector('input[name="start-date"]').value;
+    const endDate = cvForm.querySelector('input[name="end-date"]').value;
+    const degree = cvForm.querySelector('input[name="degree"]').value;
+    const university = cvForm.querySelector('input[name="university"]').value;
+    const graduationYear = cvForm.querySelector('input[name="graduation-year"]').value;
+    const skills = cvForm.querySelector('textarea[name="skills"]').value;
+
+
 
     try {
-        var currentToken = sessionStorage.getItem('token');
+        const response = await fetch('/api/cvs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                Name: cvName,
+                FullName: fullName,
+                Email: email,
+                Gender: gender,
+                Age: parseInt(age),
+                Phone: phone,
+                Jobtitle: jobTitle,
+                Company: company,
+                StartDate: startDate,
+                EndDate: endDate,
+                Degree: degree,
+                University: university,
+                GraduationYear: parseInt(graduationYear),
+                Skills: skills
+            }),
+        });
 
-        if (currentToken != null) {
-            const response = await fetch('/api/cvs', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Bearer ' + currentToken
-                },
-                body: JSON.stringify({
-                    Name: formData.get('name'), // Assuming you have an input element with name="name"
-                    email: formData.get('email'), // Assuming you have an input element with name="email"
-                    password: formData.get('password') // Assuming you have an input element with name="password"
-                })
-            });
+        if (response.ok) {
+            console.log('Successfully created CV');
+            return window.location.href = "https://localhost:7247/Home/Dashboard";
 
-            if (response.ok) {
-                window.location.href = "https://localhost:7247/Home/Dashboard";
-            } else {
-                console.error('Failed to create CV:', response.status);
-            }
+
         } else {
-            console.error('Failed to login:', loginauth.status);
+            console.error('Failed to create CV:', response.status);
         }
     } catch (error) {
         console.error('Error creating CV:', error);
     }
 });
+
+window.addEventListener('load', async () => {
+    try {
+        const response = await fetch('/api/cvs', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            },
+        });
+
+        if (response.ok) {
+            const cvsData = await response.json();
+
+            // Get the cv-list div to append the CV data
+            const cvListDiv = document.querySelector('.cv-list');
+
+            // Clear any existing CV cards
+            cvListDiv.innerHTML = '';
+
+            // Iterate through the CV data and add each CV as a new card in the cv-list div
+            cvsData.forEach(cv => {
+                const cvCard = document.createElement('div');
+                cvCard.classList.add('cv-card');
+
+                // Build the HTML for the CV card
+                cvCard.innerHTML = `
+          <h2>${cv.name}</h2>
+          <p><strong>Full Name:</strong> ${cv.fullName}</p>
+          <p><strong>Email:</strong> ${cv.email}</p>
+          <p><strong>Gender:</strong> ${cv.gender}</p>
+          <p><strong>Age:</strong> ${cv.age}</p>
+          <p><strong>Phone:</strong> ${cv.phone}</p>
+          <p><strong>Address:</strong> ${cv.address}</p>
+          <p><strong>Job Title:</strong> ${cv.jobTitle}</p>
+          <p><strong>Company:</strong> ${cv.company}</p>
+          <p><strong>Start Date:</strong> ${cv.startDate}</p>
+          <p><strong>End Date:</strong> ${cv.endDate}</p>
+          <p><strong>Degree:</strong> ${cv.degree}</p>
+          <p><strong>University:</strong> ${cv.university}</p>
+          <p><strong>Graduation Year:</strong> ${cv.graduationYear}</p>
+          <p><strong>Skills:</strong> ${cv.skills}</p>
+        `;
+
+                // Append the CV card to the cv-list div
+                cvListDiv.appendChild(cvCard);
+            });
+
+            console.log('Successfully fetched CVs');
+        } else {
+            console.error('Failed to fetch CVs:', response.status);
+        }
+    } catch (error) {
+        console.error('Error fetching CVs:', error);
+    }
+});
+
+
+//const cvForm = document.getElementById('cv-form');
+//cvForm.addEventListener('submit', async (e) => {
+//    e.preventDefault();
+
+//    const formData = new FormData(cvForm);
+
+//    try {
+//        var currentToken = sessionStorage.getItem('token');
+
+//        if (currentToken != null) {
+//            const response = await fetch('/api/cvs', {
+//                method: 'POST',
+//                headers: {
+//                    'Content-Type': 'application/x-www-form-urlencoded',
+//                    'Authorization': 'Bearer ' + currentToken
+//                },
+//                body: JSON.stringify({
+//                    Name: formData.get('name'), // Assuming you have an input element with name="name"
+//                    email: formData.get('email'), // Assuming you have an input element with name="email"
+//                    password: formData.get('password') // Assuming you have an input element with name="password"
+//                })
+//            });
+
+//            if (response.ok) {
+//                window.location.href = "https://localhost:7247/Home/Dashboard";
+//            } else {
+//                console.error('Failed to create CV:', response.status);
+//            }
+//        } else {
+//            console.error('Failed to login:', loginauth.status);
+//        }
+//    } catch (error) {
+//        console.error('Error creating CV:', error);
+//    }
+//});
 
 
 

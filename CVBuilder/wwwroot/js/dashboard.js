8b6d9f23a8b3c97431e130e76761aa74d171fe41
dist <120486581+dimitarpst@users.noreply.github.com>
2023-07-21 01:06:33 +0300

    kosta

--- CVBuilder/wwwroot/js/dashboard.js
+++ CVBuilder/wwwroot/js/dashboard.js
@@ -33,16 +33,16 @@ cvForm.addEventListener('submit', async (e) => {
     const fullName = cvForm.querySelector('input[name="fullname"]').value;
     const email = cvForm.querySelector('input[name="email"]').value;
     const gender = cvForm.querySelector('input[name="gender"]').value;
-    const age = cvForm.querySelector('input[name="age"]').value;
+/*    const age = cvForm.querySelector('input[name="age"]').value;*/
     const phone = cvForm.querySelector('input[name="phone"]').value;
     const address = cvForm.querySelector('input[name="address"]').value;
     const jobTitle = cvForm.querySelector('input[name="job-title"]').value;
     const company = cvForm.querySelector('input[name="company"]').value;
-    const startDate = cvForm.querySelector('input[name="start-date"]').value;
-    const endDate = cvForm.querySelector('input[name="end-date"]').value;
+    //const startDate = cvForm.querySelector('input[name="start-date"]').value;
+    //const endDate = cvForm.querySelector('input[name="end-date"]').value;
     const degree = cvForm.querySelector('input[name="degree"]').value;
     const university = cvForm.querySelector('input[name="university"]').value;
-    const graduationYear = cvForm.querySelector('input[name="graduation-year"]').value;
+/*    const graduationYear = cvForm.querySelector('input[name="graduation-year"]').value;*/
     const skills = cvForm.querySelector('textarea[name="skills"]').value;
 
 
@@ -59,15 +59,15 @@ cvForm.addEventListener('submit', async (e) => {
                 FullName: fullName,
                 Email: email,
                 Gender: gender,
-                Age: parseInt(age),
+/*                Age: parseInt(age),*/
                 Phone: phone,
                 Jobtitle: jobTitle,
                 Company: company,
-                StartDate: startDate,
-                EndDate: endDate,
+                //StartDate: startDate,
+                //EndDate: endDate,
                 Degree: degree,
                 University: university,
-                GraduationYear: parseInt(graduationYear),
+/*                GraduationYear: parseInt(graduationYear),*/
                 Skills: skills
             }),
         });
@@ -107,8 +107,17 @@ window.addEventListener('load', async () => {
 
                 cvCard.innerHTML = `
                 <h2>${cv.name}</h2>
+                <button class="edit-cv-btn" data-cv-id="${cv.cvId}">Edit CV</button>
                 <button class="delete-cv-btn" data-cv-id="${cv.cvId}">Delete CV</button>
-                `;
+            `;
+
+                const editButtons = document.querySelectorAll('.edit-cv-btn');
+                editButtons.forEach(editButton => {
+                    editButton.addEventListener('click', () => {
+                        const cvId = editButton.getAttribute('data-cv-id');
+                        openEditModal(cvId);
+                    });
+                });
 
                 const deleteBtn = cvCard.querySelector('.delete-cv-btn');
                 deleteBtn.addEventListener('click', (event) => {
@@ -127,7 +136,251 @@ window.addEventListener('load', async () => {
         console.error('Error fetching CVs:', error);
     }
 });
+function openEditModal(cvId) {
+    const cvModalContainer = document.getElementById('cv-modal-container');
+    const cvForm = document.getElementById('cv-form');
 
+    // Fetch the CV data for the specified ID
+    fetch(`/api/cvs/${cvId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+        },
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch CV data');
+            }
+        })
+        .then(cvData => {
+            // Fill the form fields with the retrieved CV data
+            cvForm.querySelector('input[name="cvname"]').value = cvData.Name;
+            cvForm.querySelector('input[name="fullname"]').value = cvData.FullName;
+            cvForm.querySelector('input[name="email"]').value = cvData.Email;
+            cvForm.querySelector('input[name="gender"]').value = cvData.Gender;
+            cvForm.querySelector('input[name="age"]').value = cvData.Age;
+            cvForm.querySelector('input[name="phone"]').value = cvData.Phone;
+            cvForm.querySelector('input[name="address"]').value = cvData.Address;
+            cvForm.querySelector('input[name="job-title"]').value = cvData.JobTitle;
+            cvForm.querySelector('input[name="company"]').value = cvData.Company;
+            cvForm.querySelector('input[name="start-date"]').value = cvData.StartDate;
+            cvForm.querySelector('input[name="end-date"]').value = cvData.EndDate;
+            cvForm.querySelector('input[name="degree"]').value = cvData.Degree;
+            cvForm.querySelector('input[name="university"]').value = cvData.University;
+            cvForm.querySelector('input[name="graduation-year"]').value = cvData.GraduationYear;
+            cvForm.querySelector('textarea[name="skills"]').value = cvData.Skills;
+
+            // Show the modal
+            cvModalContainer.style.display = 'block';
+        })
+        .catch(error => {
+            console.error('Error fetching CV data:', error);
+        });
+}
+
+// Add an event listener to the close button of the modal
+const closeCvModal = document.getElementById('close-cv-modal');
+closeCvModal.addEventListener('click', () => {
+    const cvModalContainer = document.getElementById('cv-modal-container');
+    cvModalContainer.style.display = 'none';
+});
+
+// Add an event listener to the Save button inside the modal
+const saveCvBtn = document.getElementById('save-cv-btn');
+saveCvBtn.addEventListener('click', async () => {
+    const cvModalContainer = document.getElementById('cv-modal-container');
+    const cvForm = document.getElementById('cv-form');
+
+    // Retrieve the data from the form
+    const cvName = cvForm.querySelector('input[name="cvname"]').value;
+    const fullName = cvForm.querySelector('input[name="fullname"]').value;
+    const email = cvForm.querySelector('input[name="email"]').value;
+    const gender = cvForm.querySelector('input[name="gender"]').value;
+    const age = cvForm.querySelector('input[name="age"]').value;
+    const phone = cvForm.querySelector('input[name="phone"]').value;
+    const address = cvForm.querySelector('input[name="address"]').value;
+    const jobTitle = cvForm.querySelector('input[name="job-title"]').value;
+    const company = cvForm.querySelector('input[name="company"]').value;
+    const startDate = cvForm.querySelector('input[name="start-date"]').value;
+    const endDate = cvForm.querySelector('input[name="end-date"]').value;
+    const degree = cvForm.querySelector('input[name="degree"]').value;
+    const university = cvForm.querySelector('input[name="university"]').value;
+    const graduationYear = cvForm.querySelector('input[name="graduation-year"]').value;
+    const skills = cvForm.querySelector('textarea[name="skills"]').value;
+
+    try {
+        const response = await fetch(`/api/cvs/${cvId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                Name: cvName,
+                FullName: fullName,
+                Email: email,
+                Gender: gender,
+                Age: parseInt(age),
+                Phone: phone,
+                Address: address,
+                JobTitle: jobTitle,
+                Company: company,
+                StartDate: startDate,
+                EndDate: endDate,
+                Degree: degree,
+                University: university,
+                GraduationYear: parseInt(graduationYear),
+                Skills: skills
+            }),
+        });
+
+        if (response.ok) {
+            console.log('Successfully updated CV');
+            cvModalContainer.style.display = 'none'; // Close the modal after successful update
+            // Optionally, you can reload the CV list to reflect the changes
+            // window.location.reload();
+        } else {
+            console.error('Failed to update CV:', response.status);
+        }
+    } catch (error) {
+        console.error('Error updating CV:', error);
+    }
+});
+
+////Edit
+
+//function fillEditModal(cvData) {
+//    const cvNameInput = document.getElementById('edit-cvname');
+//    const fullNameInput = document.getElementById('edit-fullname');
+//    const emailInput = document.getElementById('edit-email');
+
+//    cvNameInput.value = cvData.Name;
+//    fullNameInput.value = cvData.FullName;
+//    emailInput.value = cvData.Email;
+
+//    // Rest of the code to fill other input fields in the modal
+//    const genderInput = document.getElementById('edit-gender');
+///*    const ageInput = document.getElementById('edit-age');*/
+//    const phoneInput = document.getElementById('edit-phone');
+//    const addressInput = document.getElementById('edit-address');
+//    const jobTitleInput = document.getElementById('edit-job-title');
+//    const companyInput = document.getElementById('edit-company');
+//    //const startDateInput = document.getElementById('edit-start-date');
+//    //const endDateInput = document.getElementById('edit-end-date');
+//    const degreeInput = document.getElementById('edit-degree');
+//    const universityInput = document.getElementById('edit-university');
+///*    const graduationYearInput = document.getElementById('edit-graduation-year');*/
+//    const skillsInput = document.getElementById('edit-skills');
+
+//    genderInput.value = cvData.Gender;
+///*    ageInput.value = cvData.Age;*/
+//    phoneInput.value = cvData.Phone;
+//    addressInput.value = cvData.Address;
+//    jobTitleInput.value = cvData.JobTitle;
+//    companyInput.value = cvData.Company;
+//    //startDateInput.value = cvData.StartDate;
+//    //endDateInput.value = cvData.EndDate;
+//    degreeInput.value = cvData.Degree;
+//    universityInput.value = cvData.University;
+///*    graduationYearInput.value = cvData.GraduationYear;*/
+//    skillsInput.value = cvData.Skills;
+//}
+//async function openEditModal(cvId) {
+//    try {
+//        const response = await fetch(`/api/cvs/${cvId}`, {
+//            method: 'GET',
+//            headers: {
+//                'Content-Type': 'application/json',
+//                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+//            },
+//        });
+
+//        if (response.ok) {
+//            const cvData = await response.json();
+//            fillEditModal(cvData);
+//            const editModalContainer = document.getElementById('edit-cv-modal-container');
+//            editModalContainer.style.display = 'block';
+//        } else {
+//            console.error('Failed to fetch CV data for editing:', response.status);
+//        }
+//    } catch (error) {
+//        console.error('Error opening Edit CV modal:', error);
+//    }
+//}
+
+//const closeEditModalBtn = document.getElementById('close-edit-cv-modal');
+//closeEditModalBtn.addEventListener('click', () => {
+//    const editModalContainer = document.getElementById('edit-cv-modal-container');
+//    editModalContainer.style.display = 'none';
+//});
+
+//const saveEditCvBtn = document.getElementById('save-edit-cv-btn');
+//saveEditCvBtn.addEventListener('click', async (e) => {
+//    e.preventDefault();
+
+//    const cvIdInput = document.getElementById('edit-cv-id');
+//    const cvId = cvIdInput.value;
+
+//    const cvNameInput = document.getElementById('edit-cvname');
+//    const fullNameInput = document.getElementById('edit-fullname');
+//    const emailInput = document.getElementById('edit-email');
+
+//    const genderInput = document.getElementById('end-gender');
+// /*   const ageInput = document.getElementById('end-age');*/
+//    const phoneInput = document.getElementById('end-phone');
+//    const addressInput = document.getElementById('end-address');
+//    const jobTitleInput = document.getElementById('end-job-title');
+//    const companyInput = document.getElementById('end-company');
+//    //const startDateInput = document.getElementById('end-start-date');
+//    //const endDateInput = document.getElementById('end-date');
+//    const degreeInput = document.getElementById('end-degree');
+//    const universityInput = document.getElementById('end-university');
+///*    const graduationYearInput = document.getElementById('end-graduation-year');*/
+//    const skillsInput = document.getElementById('end-skills');
+
+//    const editedCV = {
+//        Name: cvNameInput.value,
+//        FullName: fullNameInput.value,
+//        Email: emailInput.value,
+//        Gender: genderInput.value,
+///*        Age: parseInt(ageInput.value),*/
+//        Phone: phoneInput.value,
+//        Address: addressInput.value,
+//        JobTitle: jobTitleInput.value,
+//        Company: companyInput.value,
+//        //StartDate: startDateInput.value,
+//        //EndDate: endDateInput.value,
+//        Degree: degreeInput.value,
+//        University: universityInput.value,
+///*        GraduationYear: parseInt(graduationYearInput.value),*/
+//        Skills: skillsInput.value
+//    };
+
+//    try {
+//        const response = await fetch(`/api/cvs/${cvId}`, {
+//            method: 'PUT',
+//            headers: {
+//                'Content-Type': 'application/json',
+//                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+//            },
+//            body: JSON.stringify(editedCV),
+//        });
+
+//        if (response.ok) {
+//            console.log('Successfully updated CV');
+//            window.location.reload();
+//        } else {
+//            console.error('Failed to update CV:', response.status);
+//        }
+//    } catch (error) {
+//        console.error('Error updating CV:', error);
+//    }
+//});
+
+//Delete
 async function deleteCV(cvId) {
   try {
     const response = await fetch(`/api/cvs/${cvId}`, {
@@ -149,97 +402,3 @@ async function deleteCV(cvId) {
     console.error('Error deleting CV:', error);
   }
 }
-
-//const cvForm = document.getElementById('cv-form');
-//cvForm.addEventListener('submit', async (e) => {
-//    e.preventDefault();
-
-//    const formData = new FormData(cvForm);
-
-//    try {
-//        var currentToken = sessionStorage.getItem('token');
-
-//        if (currentToken != null) {
-//            const response = await fetch('/api/cvs', {
-//                method: 'POST',
-//                headers: {
-//                    'Content-Type': 'application/x-www-form-urlencoded',
-//                    'Authorization': 'Bearer ' + currentToken
-//                },
-//                body: JSON.stringify({
-//                    Name: formData.get('name'), // Assuming you have an input element with name="name"
-//                    email: formData.get('email'), // Assuming you have an input element with name="email"
-//                    password: formData.get('password') // Assuming you have an input element with name="password"
-//                })
-//            });
-
-//            if (response.ok) {
-//                window.location.href = "https://localhost:7247/Home/Dashboard";
-//            } else {
-//                console.error('Failed to create CV:', response.status);
-//            }
-//        } else {
-//            console.error('Failed to login:', loginauth.status);
-//        }
-//    } catch (error) {
-//        console.error('Error creating CV:', error);
-//    }
-//});
-
-
-
-//const cvForm = document.getElementById('cv-form');
-//cvForm.addEventListener('submit', async (e) => {
-//    e.preventDefault();
-
-//    const formData = new FormData(cvForm);
-
-//    try {
-//        const loginauth = await fetch('/api/auth/login', {
-//            method: 'POST',
-//            headers: {
-//                'Content-Type': 'application/json'
-//            },
-//            body: JSON.stringify({
-//                "Email": "Test@gmail.com",
-//                "Password": "Password"
-//            }),
-//        });
-
-//        if (loginauth.ok) {
-//            const loginResponse = await loginauth.json();
-//            const currentToken = loginResponse.token;
-
-//            const response = await fetch('/api/cvs', {
-//                method: 'POST',
-//                headers: {
-//                    'Content-Type': 'application/x-www-form-urlencoded',
-//                    'Authorization': 'Bearer ' + currentToken
-//                },
-//                body: new URLSearchParams(formData),
-//            });
-
-//            if (response.ok) {
-//                const data = await response.json();
-
-//                const cvCardsContainer = document.getElementById('cv-cards-container');
-//                const cvCard = document.createElement('div');
-//                cvCard.classList.add('cv-card');
-//                cvCard.innerHTML = `
-//                    <h3>${data.fullname}</h3>
-//                    <p>${data.email}</p>
-//                    <p>${data.phone}</p>
-//                    <p>${data.address}</p>
-//                    <!-- Include other CV details as needed -->
-//                `;
-//                cvCardsContainer.appendChild(cvCard);
-//            } else {
-//                console.error('Failed to create CV:', response.status);
-//            }
-//        } else {
-//            console.error('Failed to login:', loginauth.status);
-//        }
-//    } catch (error) {
-//        console.error('Error creating CV:', error);
-//    }
-//});
--- CVBuilder/wwwroot/js/modalCV.js
+++ CVBuilder/wwwroot/js/modalCV.js
@@ -22,3 +22,51 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.remove('open-cv-modal');
     }
 });
+
+//// Add this inside the window.onload event
+//const editCvModalContainer = document.getElementById('edit-cv-modal-container');
+//const editCvForm = document.getElementById('edit-cv-form');
+
+//editCvForm.addEventListener('submit', async (e) => {
+//    e.preventDefault();
+
+//    // Get the values from the edit modal form
+//    const cvId = editCvForm.querySelector('input[name="cvId"]').value;
+//    const cvName = editCvForm.querySelector('input[name="cvname"]').value;
+//    const fullName = editCvForm.querySelector('input[name="fullname"]').value;
+//    const email = editCvForm.querySelector('input[name="email"]').value;
+//    // ... (get other input values)
+
+//    try {
+//        const response = await fetch(`/api/cvs/${cvId}`, {
+//            method: 'PUT', // Assuming your API uses PUT for updating CVs
+//            headers: {
+//                'Content-Type': 'application/json',
+//                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+//            },
+//            body: JSON.stringify({
+//                Name: cvName,
+//                FullName: fullName,
+//                Email: email,
+//                // ... (add other fields)
+//            }),
+//        });
+
+//        if (response.ok) {
+//            console.log('Successfully updated CV');
+//            // Hide the edit modal and reload the CV list
+//            editCvModalContainer.style.display = 'none';
+//            window.location.reload();
+//        } else {
+//            console.error('Failed to update CV:', response.status);
+//        }
+//    } catch (error) {
+//        console.error('Error updating CV:', error);
+//    }
+//});
+
+//const closeEditCvModal = document.getElementById('close-edit-cv-modal');
+//closeEditCvModal.addEventListener('click', () => {
+//    // Hide the edit modal if the close button is clicked
+//    editCvModalContainer.style.display = 'none';
+//});
